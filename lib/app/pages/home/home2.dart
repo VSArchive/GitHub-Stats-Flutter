6f3d563580f05c75fe28383748d4ec01cc622b5b
vijay shankar <vijaygaya056@gmail.com>
2021-01-30 21:32:36 +0530

    getUserBySearch

--- lib/app/pages/home/home2.dart
+++ lib/app/pages/home/home2.dart
@@ -1,9 +1,20 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:github_stats/model/userSearchModel.dart';
 import 'package:github_stats/services/api.dart';
 import 'package:provider/provider.dart';
 
-class HomeSearch extends StatelessWidget {
+class HomeSearch extends StatefulWidget {
+  @override
+  _HomeSearchState createState() => _HomeSearchState();
+}
+
+class _HomeSearchState extends State<HomeSearch> {
+  TextEditingController _username = TextEditingController();
+
+  String get username => _username.text;
+
   @override
   Widget build(BuildContext context) {
     final api = Provider.of<Api>(context);
@@ -18,6 +29,7 @@ class HomeSearch extends StatelessWidget {
               child: Container(
                 height: 45,
                 child: CupertinoTextField(
+                  controller: _username,
                   placeholder: "Search",
                   suffix: IconButton(
                     icon: Icon(
@@ -30,8 +42,26 @@ class HomeSearch extends StatelessWidget {
             ),
             CupertinoButton(
               child: Text('GetInfo'),
-              onPressed: () => api.getSearchUserInfo(),
-            )
+              onPressed: () => api.getSearchUserInfo(username),
+            ),
+            Container(
+                height: 600,
+                child: ListView(
+                  children: api.userFetchResult
+                      .map(
+                        (Item e) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                CachedNetworkImageProvider(e.avatarUrl),
+                          ),
+                          title: Text(
+                            e.login,
+                            style: TextStyle(fontSize: 17, color: Colors.white),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )),
           ],
         ),
       ),
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:github_stats/model/userSearchModel.dart';
 import 'package:http/http.dart' as http;
 
 class Api extends ChangeNotifier {
   final String key;
 
   Api({this.key});
-
+  List<Item> userFetchResult = [];
   Future getUserInfo() async {
     const String url = "https://api.github.com/users/vijayshankarrealdeal";
     final responseData = await http.get(url);
@@ -16,13 +19,19 @@ class Api extends ChangeNotifier {
     }
   }
 
-  Future getSearchUserInfo() async {
-    const String url = "https://api.github.com/search/users";
+  Future<List<Item>> getSearchUserInfo(String username) async {
+    final String url = "https://api.github.com/search/users?q=$username";
     final responseData = await http.get(url);
     if (responseData.statusCode == 200) {
-      print(responseData.body);
+      final extractedUser =
+          jsonDecode(responseData.body) as Map<String, dynamic>;
+      final x = UserSearchModel.fromJson(extractedUser);
+      x.items.forEach((element) {
+        userFetchResult.add(element);
+      });
     } else {
       print(responseData.statusCode);
     }
+    return userFetchResult;
   }
 }
