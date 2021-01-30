9d2dfbbe119feee6d5e8c10292ef239388056a21
vijay shankar <vijaygaya056@gmail.com>
2021-01-30 15:58:23 +0530

    sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'login.dart';
+import 'package:github_stats/app/navigation/change.dart';
+import 'package:github_stats/app/navigation/homepage.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      home: ChangeNotifierProvider<ChangeofPage>(
+          create: (context) => ChangeofPage(), child: MaterialHomePage()),
     );
   }
 }
