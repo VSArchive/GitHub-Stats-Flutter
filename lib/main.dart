7438d615cf681a866a6346b5318d87c85b71fbc3
vijay shankar <vijaygaya056@gmail.com>
2021-01-30 23:48:04 +0530

    only to fill login Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:github_stats/app/navigation/change.dart';
 import 'package:github_stats/app/navigation/homepage.dart';
+import 'package:github_stats/credentials/login.dart';
 import 'package:github_stats/services/api.dart';
+import 'package:github_stats/services/firebaes-auth.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MultiProvider(providers: [
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthLogic>(create: (context) => AuthLogic()),
         ChangeNotifierProvider<ChangeofPage>(
             create: (context) => ChangeofPage()),
-        ChangeNotifierProvider<Api>(create: (context) => Api()),
-      ], child: MaterialHomePage()),
+      ],
+      child: StreamBuilder<Object>(
+          stream: Provider.of<AuthLogic>(context).onAuthChanges,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              UserExtension user = snapshot.data;
+              if (user == null) {
+                return MaterialApp(
+                  home: Login(),
+                );
+              } else {
+                return MultiProvider(
+                  providers: [
+                    ChangeNotifierProvider<Api>(create: (context) => Api()),
+                  ],
+                  child: MaterialApp(home: MaterialHomePage()),
+                );
+              }
+            } else {
+              return CupertinoActivityIndicator();
+            }
+          }),
     );
   }
 }
--- lib/services/firebaes-auth.dart
+++ lib/services/firebaes-auth.dart
@@ -1,5 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 abstract class AuthID {}
 
-class AuthLogic extends AuthID with ChangeNotifier {}
+class AuthLogic extends AuthID with ChangeNotifier {
+  final _auth = FirebaseAuth.instance;
+  UserExtension _userFromFirebase(user) {
+    if (user == null) {
+      return null;
+    } else {
+      return UserExtension(uid: user.uid, email: user.email);
+    }
+  }
+
+  Stream get onAuthChanges => _auth.authStateChanges().map(_userFromFirebase);
+}
+
+class UserExtension {
+  final String uid;
+  final String email;
+  UserExtension({this.uid, this.email});
+}
