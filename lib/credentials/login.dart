a2edab7a0dbb19fe67746aa7884cda62406f04f6
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-02-28 14:59:17 +0530

    fonts

--- lib/credentials/login.dart
+++ lib/credentials/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:github_stats/services/firebaes-auth.dart';
 import 'package:provider/provider.dart';
@@ -7,13 +8,43 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthLogic>(context);
     return Container(
-      color: Colors.blue,
-      child: Center(
-        child: FlatButton(
-          child: Text('Login'),
-          onPressed: () => auth.signInWithGitHub(context),
+      color: Color.fromRGBO(255, 255, 255, 1),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ///Add github Image
+            Center(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: double.infinity,
+                child: CupertinoButton(
+                  color: Color.fromRGBO(27, 30, 35, 1),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                        fontFamily: 'SF-Pro-Text-Semibold', fontSize: 17),
+                  ),
+                  onPressed: () => auth.signInWithGitHub(context),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+// appBar: CupertinoNavigationBar(
+//         previousPageTitle: 'Chats',
+//         backgroundColor: Color.fromRGBO(249, 249, 249, 0.94),
+//         middle: Text(
+//           makeTitle(widget.x['name']),
+//           style: TextStyle(
+//               fontFamily: 'SF-Pro-Text-Semibold',
+//               fontSize: 17,
+//               color: Color.fromRGBO(0, 0, 0, 1)),
+//         ),
+//       ),
