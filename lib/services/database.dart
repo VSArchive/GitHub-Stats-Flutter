8e406455e5fbaec21d7f877a112572ea2e6a5884
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-01 18:53:27 +0530

    firecloudadded

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,8 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:github_stats/model/CloudUserData.dart';
 
 class Database {
+  final String uid;
   final _refrence = FirebaseFirestore.instance;
-  Future<void> createUser({CloudUserData data,String uid}) async {
-    
+
+  Database({this.uid});
+  Stream<List<CloudUserData>> data() {
+    return _refrence
+        .collection('User')
+        .where('uid', isEqualTo: uid)
+        .snapshots()
+        .map((e) =>
+            e.docs.map((ed) => CloudUserData.fromJson(ed.data())).toList());
   }
 }
